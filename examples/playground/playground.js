a397982b7a22ef4e1d45c9b6a778613dcbac9448
Liliya Mykhailova <liliya.pikiner@gmail.com>
2021-08-16 12:05:01 +0300

    updated demo v2

--- examples/playground/playground.js
+++ examples/playground/playground.js
@@ -11,7 +11,7 @@
         },
         defaults: {
             account: 'csdemo',
-            imgName: 'cb_d2X2Th_Kids_RWL_BdrmBoy_01_201803',
+            imgName: 'CB_DLP_201903_ColorLP_BlushRomance_Hero',
             imgClass: 'js-poi-img',
             containerClass: 'js-poi-img-container',
             basePath: 'https://www.crateandbarrel.com/',
@@ -104,6 +104,33 @@
                                     settings.$target.classList.remove('hovered');
                                 }
                             }
+                        ],
+                        polygonCallbacks: [
+                            {
+                                target: "*",
+                                action: "click",
+                                callback: function (evt, settings) {
+                                    debugger;
+                                    window.open(
+                                        opts.basePath + settings.polygon.target,
+                                        '_blank'
+                                    )
+                                }
+                            },
+                            {
+                                target: "*",
+                                action: 'mouseover',
+                                callback: function (evt, settings) {
+                                    settings.$target.classList.add('hovered');
+                                }
+                            },
+                            {
+                                target: "*",
+                                action: 'mouseout',
+                                callback: function (evt, settings) {
+                                    settings.$target.classList.remove('hovered');
+                                }
+                            }
                         ]
                     }
                 ]
@@ -111,36 +138,90 @@
             poi.init();
         },
         destroyPOI: function () {
-            $('.js-img-container').empty();
+            $('.js-img-container').find('svg').each(function () {
+                $(this).remove();
+            });
+            $('.js-img-container').find('div').each(function () {
+                $(this).remove();
+            })
         },
         reapplyImg: function (defaults, opts) {
-            $('.js-img-container').append('<img src="' + defaults.path + 'i/' +
+            var $sources = document.getElementsByTagName('source');
+            var $img = document.getElementsByTagName('img');
+            $sources = Array.from($sources);
+
+            $sources = $sources.concat($img && $img[0]);
+
+            $img && $img[0] && $img[0].setAttribute('src', defaults.path + 'i/' +
                 (opts ? opts.account : defaults.account) + '/' +
-                (opts ? opts.imgName : defaults.imgName) + '" />');
-        },
-        getData: function (opts, callback) {
-            var self = this;
-            $.ajax({
-                url: (opts.path || 'http://i1.adis.ws/') + 'i/' + opts.account + '/' + opts.imgName + '.js?v=' + new Date().getTime() + '&metadata=true',
-                jsonp: "func",
-                dataType: "jsonp",
-                success: function (response) {
-                    var resp = false;
-                    if (!response || !response.metadata || !response.metadata.hotSpots || response.metadata.hotSpots.hotSpots.list.length < 1) {
-                        resp = false;
+                (opts ? opts.imgName : defaults.imgName));
+
+            $sources.forEach(function (el) {
+                var set = el && el.getAttribute('srcset');
+                set = set.split(', ');
+
+                set = set.map(function (el) {
+                    var paths = el.split('?');
+
+                    paths[0] = defaults.path + 'i/' +
+                        (opts ? opts.account : defaults.account) + '/' +
+                        (opts ? opts.imgName : defaults.imgName);
+
+                    if (paths[1]) {
+                        paths[1] = paths[1].replace(defaults.imgName, opts.imgName)
                     }
 
-                    else {
-                        resp = response;
-                    }
+                    el = paths.join('?');
 
-                    return callback.call(self, resp);
-                },
-                error: function () {
-                    return callback.call(self, false);
-                }
+                    return el;
+                });
+
+                set = set.join(', ');
+
+                el.setAttribute('srcset', set)
             });
         },
+        /*  getData: function (opts, callback) {
+              var self = this;
+              var queryStr = opts.query || '';
+              var query = opts.imgName.includes('?') ? '&X-Amp-Trace=true&v=' + new Date().getTime() : '?' + queryStr + '&X-Amp-Trace=true&v=' + new Date().getTime();
+
+              $.ajax({
+                  url: (opts.path || 'http://i1.adis.ws/') + 'i/' + opts.account + '/' + opts.imgName + query,
+                  success: function (response) {
+                      var translate = response.find(function (el) {
+                          return el.type === 'translate';
+                      });
+                      if (!translate || !translate.data || !translate.data.output || !translate.data.output.layerCommand) {
+                          return false;
+                      }
+                      var metadata = translate.data.output.layerCommand.metadata;
+                      var layerCommand = translate.data.output.layerCommand;
+                      var childlayers = translate.data.output.childlayers;
+                      var canvas = translate.data.output.layerCommand.info.canvas;
+
+                      if (!metadata) {
+                          metadata = childlayers.find(function (el) {
+                              return el.layerCommand.metadata;
+                          });
+
+                          if (metadata && metadata.layerCommand) {
+                              layerCommand = metadata.layerCommand;
+                              metadata = metadata.layerCommand.metadata;
+                          }
+                      }
+
+                      return callback.call(self, {
+                          metadata: metadata,
+                          canvas: canvas,
+                          layerCommand: layerCommand
+                      });
+                  },
+                  error: function () {
+                      return callback.call(self, false);
+                  }
+              });
+          },*/
 
         //Panel behavior
         panelToggle: function ($panelNav, $panel, callback) {
@@ -148,6 +229,7 @@
             $panelNav.click(function (e) {
                 e.preventDefault();
                 $panel.toggle();
+                $panelNav.toggleClass('active');
 
                 if (typeof callback === 'function') {
                     callback.call(self);
@@ -177,26 +259,33 @@
                 return false;
             }
 
-            self.getData(settings, function (jsonData) {
-                self.destroyPOI();
+            //self.getData(settings, function (jsonData) {
+            self.destroyPOI();
+            $panelNav.removeClass('with-error');
+            self.reapplyImg(self.defaults, settings);
+            self.initPOI(settings);
 
-                if (!jsonData) {
-                    $panelNav.addClass('with-error');
-                    self.reapplyImg(self.defaults);
-                    self.initPOI(self.defaults);
-                }
+            var $picker = document.getElementById("colorPicker");
+            var picker = tinycolorpicker($picker);
+            picker.setColor('#ececec');
 
-                else {
-                    $panelNav.removeClass('with-error');
-                    self.reapplyImg(self.defaults, settings);
-                    self.initPOI(settings, jsonData);
-                }
-            });
+            Object.assign(self.defaults, settings)
+            // });
         },
-        panelInit: function ($panelNav, $panel, $panelButton, $panneltoHide) {
+        panelInit: function ($panelNav, $panel, $panelButton, $panneltoHide, $panelDocs, $panelDocsContainer, $codeNav) {
             var self = this;
             self.panelToggle($panelNav, $panel, function () {
                 $panneltoHide.hide();
+                $panelDocsContainer.hide();
+                $panelDocs.removeClass('active');
+                $codeNav.removeClass('active');
+            });
+
+            self.panelToggle($panelDocs, $panelDocsContainer, function () {
+                $panneltoHide.hide();
+                $panel.hide();
+                $panelNav.removeClass('active');
+                $codeNav.removeClass('active');
             });
 
             $panelButton.click(function (e) {
@@ -208,9 +297,9 @@
         appendScriptStyles: function (callback) {
             var self = this;
 
-            var style = document.createElement("link")
-            style.setAttribute("rel", "stylesheet")
-            style.setAttribute("type", "text/css")
+            var style = document.createElement("link");
+            style.setAttribute("rel", "stylesheet");
+            style.setAttribute("type", "text/css");
             style.setAttribute("href", self.defaults.poiCssSrc);
             document.head.appendChild(style);
 
@@ -261,11 +350,14 @@
                 document.execCommand('copy');
             });
         },
-        codeCopyInit: function ($codeNav, $codePanel, $codeCopyButton, $textarea, $panelToHide) {
+        codeCopyInit: function ($codeNav, $codePanel, $codeCopyButton, $textarea, $panelToHide, $panelDocsContainer, $panelDocs, $panelNav) {
             var self = this;
 
             self.panelToggle($codeNav, $codePanel, function () {
                 $panelToHide.hide();
+                $panelDocsContainer.hide();
+                $panelDocs.removeClass('active');
+                $panelNav.removeClass('active');
                 self.constructCopyCode($textarea)
             });
             self.copyToClipboard($codeCopyButton, $textarea);
@@ -275,18 +367,48 @@
             var $panelNav = $('.js_panel_nav');
             var $panel = $('.js_panel');
             var $panelButton = $('.js_panel_submit');
+            var $panelCancelButton = $('.js_panel_submit_cancel');
+            var $panelDocs = $('.js_docs');
 
             var $codeNav = $('.js_code_nav');
             var $codePanel = $('.js_code_panel');
             var $codeCopyButton = $('.js_copy_clipboard');
-            var $codeTextarea = $('.js_copy_textarea')
+            var $codeTextarea = $('.js_copy_textarea');
+            var $panelDocsContainer = $('.code-docs-panel');
+            var $imgWrapper = $('.img-wrapper');
 
-            self.getData(self.defaults, function (jsonData) {
-                self.defaults.jsonData = jsonData
-                self.initPOI(self.defaults);
+            self.initPOI(self.defaults);
+
+            $panelCancelButton.on('click', function (e) {
+                e.preventDefault();
+                $panel.toggle();
+                $panelNav.removeClass('active');
             });
-            self.panelInit($panelNav, $panel, $panelButton, $codePanel);
-            self.codeCopyInit($codeNav, $codePanel, $codeCopyButton, $codeTextarea, $panel);
+
+            $imgWrapper.on('click', function (e) {
+                e.preventDefault();
+                $panel.hide();
+                $codePanel.hide();
+                $panelDocsContainer.hide();
+                $panelNav.removeClass('active');
+                $codeNav.removeClass('active');
+                $panelDocs.removeClass('active');
+            });
+
+            /* self.getData(self.defaults, function (jsonData) {
+                 if (jsonData.metadata) {
+                     self.defaults.jsonData = jsonData.metadata;
+                 }
+                 if (jsonData.canvas) {
+                     self.defaults.canvas = jsonData.canvas;
+                 }
+                 if (jsonData.layerCommand) {
+                     self.defaults.layerCommand = jsonData.layerCommand;
+                 }
+                 self.initPOI(self.defaults);
+             });*/
+            self.panelInit($panelNav, $panel, $panelButton, $codePanel, $panelDocs, $panelDocsContainer, $codeNav);
+            self.codeCopyInit($codeNav, $codePanel, $codeCopyButton, $codeTextarea, $panel, $panelDocsContainer, $panelDocs, $panelNav);
         }
     }
 }(jQuery));
